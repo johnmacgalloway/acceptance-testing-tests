fca6af1c4486ed57935a9f162feff28500e62089
johnmacgalloway <johnmacg@andersonlawrence.co.uk>
2016-09-22 17:22:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { fetch } from './ducks/forecast';
 import Weather from './components/weather';
 import createStore from './store';
 
-const CITY = 'Glasgow';
+const CITY = 'Perth';
 
 const store = createStore()
     , root = document.getElementById('root');
